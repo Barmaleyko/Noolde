4272f952f7be3a99e9917131d7aaef46f27ec124
OlshevskyR <olshevsky.r@gmail.com>
2013-11-23 05:43:42 +0200

    FOSUserBundle Configured

---
+++ src/RoSky/Bundle/NoodleBundle/Entity/User.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace RoSky\Bundle\NoodleBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="User")
+ */
+class User extends BaseUser
+{
+    /**
+     * @var integer
+     */
+    protected $id;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function _construct()
+    {
+        parent::_construct();
+    }
+}
